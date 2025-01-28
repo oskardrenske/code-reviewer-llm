bf478adcee71b61ecc5f73b1a46b3ff2eccb47ff
oskardrenske <oskar.drenske@gmail.com>
2025-01-28 07:17:09 +0100

    Logging and error handling

--- code_reviewer.py
+++ code_reviewer.py
@@ -13,30 +13,40 @@ class CodeReviewer:
         self.result_dir = file_handler.create_review_results_dir(
             self.path_to_check, self.timestamp
         )
-
-        file_handler.init_result_file(
-            self.result_dir, "review.txt", self.path_to_check, self.timestamp
+        self.result_file = file_handler.init_result_file(
+            self.result_dir,
+            settings.review_file_name,
+            self.path_to_check,
+            self.timestamp,
         )
+        self.file = None
+        logger.info("Setup complete")
 
     def start_review(self):
         for file in self.files_to_check:
-            logger.warning(file)
+            self.file = file
             review = self.llm(file_handler.read_file(file))
-
             file_handler.write_rewiev_result(
                 self.result_dir, settings.review_file_name, review, file
             )
 
     def llm(self, file_content):
-        ollama_response = ollama.chat(
-            model=settings.llm_model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": settings.system_prompt,
-                },
-                {"role": "user", "content": f"{settings.user_prompt} {file_content}"},
-            ],
-            options={"temperature": settings.temperature},
-        )
-        return ollama_response["message"]["content"]
+        try:
+            ollama_response = ollama.chat(
+                model=settings.llm_model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": settings.system_prompt,
+                    },
+                    {
+                        "role": "user",
+                        "content": f"{settings.user_prompt} {file_content}",
+                    },
+                ],
+                options={"temperature": settings.temperature},
+            )
+            return ollama_response["message"]["content"]
+        except Exception as e:
+            logger.error(e)
+            return f"Error when reviewing {self.file}: {e}"
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from loguru import logger
 from code_reviewer import CodeReviewer
 
 
 def main():
+    logger.info("start")
     reviewer = CodeReviewer()
     reviewer.start_review()
+    logger.info(f"Finished, read report file: {reviewer.result_file}")
 
 
 if __name__ == "__main__":
